aca04bf47ab8a95b267997a4a1a01d97a20ef917
Udoka Nwosu <lookbeyond@tuta.io>
2022-03-16 19:36:24 +0000

    update name

--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module github.com/nwosuudoka/go-gecko
 
 go 1.17
 
-require (
-	github.com/superoo7/go-gecko v1.0.0
-	gopkg.in/h2non/gock.v1 v1.0.14
-)
+require gopkg.in/h2non/gock.v1 v1.0.14
 
 require github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
--- v3/v3.go
+++ v3/v3.go
@@ -8,8 +8,8 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/superoo7/go-gecko/format"
-	"github.com/superoo7/go-gecko/v3/types"
+	"github.com/nwosuudoka/go-gecko/format"
+	"github.com/nwosuudoka/go-gecko/v3/types"
 )
 
 var baseURL = "https://api.coingecko.com/api/v3"
